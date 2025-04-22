c3f398326f675f2d633d45e3f121da913f80b333
owllwo98 <sloopy98@naver.com>
2025-04-22 12:17:41 +0900

    Initial Commit

---
+++ TCABasic/TCABasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCABasicApp.swift
+//  TCABasic
+//
+//  Created by 변정훈 on 4/22/25.
+//
+
+import SwiftUI
+
+@main
+struct TCABasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
